829bfa370fb27591c20e80a3128bbfd22e5d1aca
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-10-19 16:02:08 +0200

    dynamic add more

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -90,6 +90,7 @@
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript">
+        //dynamic add more
     var i = 0;
        
     $("#add").click(function(){
