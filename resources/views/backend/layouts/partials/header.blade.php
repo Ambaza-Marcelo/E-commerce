f1a92182c6d8ceb2022ea76a9bbbd736c123d08e
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-11-07 07:40:12 +0100

    language select

--- resources/views/backend/layouts/partials/header.blade.php
+++ resources/views/backend/layouts/partials/header.blade.php
@@ -9,7 +9,7 @@
             <ul class="notification-area pull-right">
                 <li id="full-view"><i class="ti-fullscreen"></i></li>
                 <li id="full-view-exit"><i class="ti-zoom-out"></i></li>
-                <li>
+                <li><!-- language select-->
                     <select class="form-control changeLang">
                             <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>@lang('messages.english')</option>
 
