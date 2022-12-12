982b12db1ff9918803ae2ea45f44a371db7cd713
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-12-12 15:15:15 +0100

    404 message code

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('error-content')
+	<!-- 403 message-->
     <h2>404</h2>
     <p>@lang('messages.404')</p>
     <a href="{{ route('admin.dashboard') }}">@lang('messages.back_to_dashboard')</a>
