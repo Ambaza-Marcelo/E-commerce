e43e11ab8008688560f214d9019118ec99ef752b
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-12-19 14:14:05 +0100

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
+                <!-- login form -->
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
